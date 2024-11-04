7e94159280db5b7b6e069197681b19d072054f44
ILGIZ SAB <ilgizastr@gmail.com>
2024-11-04 20:11:37 +0300

    Task solved. Test success

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,20 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+
+        HelloWorld beanHelloWorld1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(beanHelloWorld1.getMessage());
+
+        HelloWorld beanHelloWorld2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanHelloWorld2.getMessage());
+
+        System.out.println("Compare beans \"helloWorld\": " + (beanHelloWorld1.equals(beanHelloWorld2)));
+
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Compare beans \"cat\": " + (beanCat1.equals(beanCat2)));
     }
 }
